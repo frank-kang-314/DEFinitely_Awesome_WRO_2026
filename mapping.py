fa0e62b55f2264dbe5dec728ad6bda4b9506adcf
Frank Kang <fronkdetonk@gmail.com>
2026-08-19 09:40:04 -0500

    Add description of purpose of each class in code

--- mapping.py
+++ mapping.py
@@ -1,48 +1,32 @@
 """
-Dimensions of track: 
-300 cm by 300 cm
+OVERALL HIERARCHY OF THE CODE: 
 
-Height of walls: 
-10cm
+Main class: Car (General controls)
+    * Start car
+    * Leave parking lot (if applicable)
+    * Perform main sequence
+    * Park (if applicable)
+    * Stop
+
+Three systems: Steering, Sensors (Hardware), Map (Software)
+
+Steering: 
+    * Turn
+    * Drive forward / backward
+    * Stop
+
+Sensors: 
+    * Read ultrasonic sensor and camera data
+    * Output the data to other functions / classes
+
+Map: 
+    * Create walls
+    * Record seen obstacles
+    * Update car position
+    * Send instructions to steering
 """
 
-track_width = 300
 
-#Coordinates range from (0,0) (bottom left) to (300,300) (top right)
-
-block_positions = [
-    #bottom side
-    (100,40),
-    (100,60),
-    (150,40),
-    (150,60),
-    (200,40),
-    (200,60),
-
-    #left side
-    (40,100),
-    (60,100),
-    (40,150),
-    (60,150),
-    (40,200),
-    (60,200),
-
-    #top side
-    (100,240),
-    (100,260),
-    (150,240),
-    (150,260),
-    (200,240),
-    (200,260),
-
-    #right side
-    (240,100),
-    (260,100),
-    (240,150),
-    (260,150),
-    (240,200),
-    (260,200)
-]
 
 rect_types = {
     "no_collide_outer": "NO_COLLIDE_OUTER", #things you can't hit the outside of, like traffic signs
@@ -65,42 +49,51 @@ class Rect:
         """
         
 class Wall(Rect):
-    def __init__(self, **type: str):
-        if type == "outer":
+    def __init__(self, **location: str):
+        if location == "outer":
             self.bottom_left_corner = (0,0)
             self.width = 300
             self.height = 300
             self.type = rect_types["no_collide_inner"]
             
 
-class Block:
+class Block(Rect):
     def __init__(self, color, position):
         self.color = color
         self.position = position
     def update_position(self, **data):
         pass
 
-class ParkingLot():
+class ParkingLot(Rect):
     def __init__(self):
         pass
 
 class Map:
     __laps = 0 #Private variable
+    track_width = 300
+
+    #Coordinates range from (0,0) (bottom left) to (300,300) (top right)
+
+    block_positions =((100,40), (100,60), (150,40), (150,60), (200,40), (200,60), (40,100), (60,100), (40,150), (60,150), (40,200), (60,200), (100,240), (100,260), (150,240), (150,260), (200,240), (200,260), (240,100), (260,100), (240,150), (260,150), (240,200), (260,200))
+
     def __init__(self):
         pass
     def setup(self):
         #This is the list where all future objects on the map will be added
         self.objects = []
 
-        self.objects.append(Wall(type = "outer"))
+        self.objects.append(Wall(location = "outer"))
 
         #Set challenge type (open/obstacle) and direction (clockwise/counterclockwise)
 
         #Set current position
 
         #Start looking for obstacles
-
         pass
+    def add(self):
+        #Add new objects as the car discovers more of the track
+        pass
+        
     def recalibrate(self):
         pass
     def check_laps(self):
@@ -111,16 +104,30 @@ class Map:
 class Steering:
     def __init__(self):
         pass
-    def turn(angle: int):
+
+    def start(self):
+        pass
+
+    def turn(self, angle: int):
         #Straight: 110
         #Left: 150
         #Right: 60
         pass
 
-    def move(*, direction: str, speed: float):
+    def move(self, *, direction: str, speed: float):
         pass
 
-    def brake():
+    def brake(self):
+        pass
+
+class Sensors:
+    def __init__(self):
+        pass
+    def read_ultrasonic(self):
+        pass
+    def read_camera(self):
+        pass
+    def return_data(self):
         pass
 
 class Car:
@@ -130,8 +137,13 @@ class Car:
     def __init__(self):
         pass
     def start(self):
+        steering = Steering()
+
+        sensors = Sensors()
+
         map = Map()
         map.setup()
+
     def leave_parking_lot(self):
         pass
     def park(self):
